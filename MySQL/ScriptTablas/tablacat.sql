1b5c7e666616ca2539a03f7441fe021c94d22095
salo <salomonsanchezmarin95@gmail.com>
2022-10-10 13:51:42 -0500

    Se modifican los scripts de las bases de datos

--- MySQL/ScriptTablas/tablacat.sql
+++ MySQL/ScriptTablas/tablacat.sql
@@ -1,6 +1,6 @@
-SELECT * FROM mexishopdb.categorias;
-INSERT INTO `mexishopdb`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('1', 'Alebrijes');
-INSERT INTO `mexishopdb`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('2', 'Barro y Cerámica');
-INSERT INTO `mexishopdb`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('3', 'Joyería');
-INSERT INTO `mexishopdb`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('4', 'Ropa');
-INSERT INTO `mexishopdb`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('5', 'Calzado');
+
+INSERT INTO `mexishop2db`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('1', 'Alebrijes');
+INSERT INTO `mexishop2db`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('2', 'Barro y Ceramica');
+INSERT INTO `mexishop2db`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('3', 'Joyeria');
+INSERT INTO `mexishop2db`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('4', 'Ropa');
+INSERT INTO `mexishop2db`.`categorias` (`cate_id`, `nombre_cat`) VALUES ('5', 'Calzado');
